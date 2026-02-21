871f766ebdaa5ffa94aa23b1243028cb43e387bd
vetalmel0092-cloud <vetalmel0092@gmail.com>
2026-02-21 21:00:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ try:
         Args:
             message (telebot.types.Message): The message object.
         """
-        bot.reply_to(message, "Hello! I'm a simple Telegram bot.")
+        bot.reply_to(message, "Вітаю! Це Bitcoin Game 💰")
 
     @bot.message_handler(func=lambda msg: True)
     def echo_all(message):
